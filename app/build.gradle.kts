19e3129508b663b0e08f762fdbfc7db684ae725c
Halil Ozel <halilozel1903@gmail.com>
2023-11-18 17:53:13 +0300

    Test data removed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,15 +40,10 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation ("androidx.media3:media3-exoplayer:1.2.0")
     implementation ("androidx.media3:media3-ui:1.2.0")
     implementation ("androidx.media3:media3-exoplayer-hls:1.2.0")
